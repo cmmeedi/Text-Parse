9a0cec193ab54d34c0a0542eff20f8188aa6ce93
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2021-11-21 13:26:40 -0700

    Add files via upload

---
+++ ProgrammingProject3/ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ProgrammingProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GetTextButton = new System.Windows.Forms.Button();
+            this.WordsFromFileLabel = new System.Windows.Forms.Label();
+            this.FirstWordLabel = new System.Windows.Forms.Label();
+            this.openFileDialogue = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SaveFileButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.LastWord = new System.Windows.Forms.Label();
+            this.LongestWord = new System.Windows.Forms.Label();
+            this.MostVowels = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GetTextButton
+            // 
+            this.GetTextButton.Location = new System.Drawing.Point(9, 423);
+            this.GetTextButton.Name = "GetTextButton";
+            this.GetTextButton.Size = new System.Drawing.Size(75, 23);
+            this.GetTextButton.TabIndex = 1;
+            this.GetTextButton.Text = "Get Text";
+            this.GetTextButton.UseVisualStyleBackColor = true;
+            this.GetTextButton.Click += new System.EventHandler(this.GetTextButton_Click);
+            // 
+            // WordsFromFileLabel
+            // 
+            this.WordsFromFileLabel.AutoSize = true;
+            this.WordsFromFileLabel.Location = new System.Drawing.Point(9, 245);
+            this.WordsFromFileLabel.Name = "WordsFromFileLabel";
+            this.WordsFromFileLabel.Size = new System.Drawing.Size(80, 13);
+            this.WordsFromFileLabel.TabIndex = 2;
+            this.WordsFromFileLabel.Text = "Words from File";
+            // 
+            // FirstWordLabel
+            // 
+            this.FirstWordLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FirstWordLabel.Location = new System.Drawing.Point(12, 28);
+            this.FirstWordLabel.Name = "FirstWordLabel";
+            this.FirstWordLabel.Size = new System.Drawing.Size(80, 13);
+            this.FirstWordLabel.TabIndex = 4;
+            // 
+            // openFileDialogue
+            // 
+            this.openFileDialogue.Title = "Pick a File Vato";
+            this.openFileDialogue.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // SaveFileButton
+            // 
+            this.SaveFileButton.Location = new System.Drawing.Point(90, 423);
+            this.SaveFileButton.Name = "SaveFileButton";
+            this.SaveFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveFileButton.TabIndex = 5;
+            this.SaveFileButton.Text = "Save File";
+            this.SaveFileButton.UseVisualStyleBackColor = true;
+            this.SaveFileButton.Click += new System.EventHandler(this.SaveFileButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 261);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 160);
+            this.listBox1.TabIndex = 6;
+            // 
+            // LastWord
+            // 
+            this.LastWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LastWord.Location = new System.Drawing.Point(12, 53);
+            this.LastWord.Name = "LastWord";
+            this.LastWord.Size = new System.Drawing.Size(80, 13);
+            this.LastWord.TabIndex = 7;
+            // 
+            // LongestWord
+            // 
+            this.LongestWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LongestWord.Location = new System.Drawing.Point(12, 78);
+            this.LongestWord.Name = "LongestWord";
+            this.LongestWord.Size = new System.Drawing.Size(80, 13);
+            this.LongestWord.TabIndex = 8;
+            // 
+            // MostVowels
+            // 
+            this.MostVowels.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MostVowels.Location = new System.Drawing.Point(12, 100);
+            this.MostVowels.Name = "MostVowels";
+            this.MostVowels.Size = new System.Drawing.Size(172, 133);
+            this.MostVowels.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 459);
+            this.Controls.Add(this.MostVowels);
+            this.Controls.Add(this.LongestWord);
+            this.Controls.Add(this.LastWord);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.SaveFileButton);
+            this.Controls.Add(this.FirstWordLabel);
+            this.Controls.Add(this.WordsFromFileLabel);
+            this.Controls.Add(this.GetTextButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dammit Vato";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button GetTextButton;
+        private System.Windows.Forms.Label WordsFromFileLabel;
+        private System.Windows.Forms.Label FirstWordLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialogue;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Button SaveFileButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label LastWord;
+        private System.Windows.Forms.Label LongestWord;
+        private System.Windows.Forms.Label MostVowels;
+    }
+}
+
---
+++ ProgrammingProject3/ProgrammingProject3/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace ProgrammingProject3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetTextButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Creating an ArrayList to hold a scalable list
+                ArrayList allWords = new ArrayList();
+
+                //Declaring a variable to store the word
+                string wordly;
+                string maximum;
+                int maximumInt;
+
+
+                ArrayList vowels = new ArrayList();
+
+
+
+
+                //Declaring the StreamReader variable
+                StreamReader inputFile;    
+                
+                //==============================================================================================
+                //Asks the user to open a file
+                if(openFileDialogue.ShowDialog() == DialogResult.OK)
+                {
+                    //Assigns the input file variable to the user selected file
+                    inputFile = File.OpenText(openFileDialogue.FileName);
+
+                    //Creates a loop printing out each line until the end of the file.
+                    while (!inputFile.EndOfStream)
+                    {
+                        //Adds line to Placeholder "wordly" and converts them to lowercase.
+                        wordly = inputFile.ReadLine().ToLower();
+
+                        //Adds current "wordly" to "allWords" ArrayList
+                        allWords.Add(wordly);
+
+                        //Adds current "wordly" to listbox1
+                        listBox1.Items.Add(wordly);
+                    }
+
+                //========================================================================================================================================
+
+                    //This sorts the "allWords" ArrayList in alphabetical order
+                    allWords.Sort();
+
+                    //then pulls the first word
+                    FirstWordLabel.Text = allWords[0].ToString();
+
+                    //Afterwards we reverse the list
+                    allWords.Reverse();
+
+                    //and pull the first word
+                    LastWord.Text = allWords[0].ToString();
+
+                    //This loop iterates through the arraylist
+                    for (int i = 0; i <= allWords.Count; i++)
+                    {
+                        //Creates a placeholder for the length of the iterated string
+                        int length = allWords[i].ToString().Length;
+
+                        //and a placeholder of the one being compared
+                        int big = allWords[0].ToString().Length;
+
+                        //They get plugged into this loop and get compared
+                        if (length > big)
+                        {
+                            //The maximum placeholder gets set to the longest word
+                            maximum = allWords[i].ToString();
+
+                            //and assigned to the LongestWord label
+                            LongestWord.Text = maximum;
+                        }
+                    }
+
+                //==========================================================================================================================================
+
+                    //This loop iterates through the arraylist
+                    for (int i = 0; i <= allWords.Count; i++)
+                    {
+                        //This is variable is a placeholder for how many vowels there are in the word
+                        int vowel = 0;
+
+                        //An array list is created to hold the vowels corresponding with the same position as the word in "allWords"
+
+                        //This for loop iterates through each letter of the current word looking for vowels
+                        for ( int j = 0; j < allWords[i].ToString().Length; j++)
+                        {
+                            if (j.ToString().Contains('a'))
+                            {
+                                //if a vowel is found then the vowel variable is incremented
+                                vowel++;
+
+                                if (j.ToString().Contains('e'))
+                                {
+                                    vowel++;
+
+                                    if (j.ToString().Contains('i'))
+                                    {
+                                        vowel++;
+
+                                        if (j.ToString().Contains('o'))
+                                        {
+                                            vowel++;
+
+                                            if (j.ToString().Contains('u'))
+                                            {
+                                                vowel++;
+
+                                                //The amount of vowels is stored in a different array list with the same position as the "allWords" arraylist
+                                                vowels.Add(vowel);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    //=====================================================================================================================================
+
+                        //This for loop iterates through the vowels arrayList
+                        for(int big = 0; big < vowels.Count; big++)
+                        {
+                            //Setting the length variable to the current selection
+                            int length = (int)vowels[big];
+
+                            //and the max variable to the 0 position
+                            int max = (int)vowels[0];
+
+                            //Then compares the two
+                            if(length > max)
+                            {
+                                //Then we set the biggest one to the index of the "allWords" list and set that to the maximum variable
+                                maximum = allWords[(int)vowels[big]].ToString();
+
+                                //and print that to the most vowels label
+                                MostVowels.Text = maximum;
+
+                                //I'm not understanding why this won't print
+                            }
+                        }
+                    }
+
+                //=========================================================================================================================================
+
+                    //Closes the File
+                    inputFile.Close();
+
+                }
+                else
+                {
+                    //Cancel Message
+                    MessageBox.Show("Operation Canceled");
+                }   
+            }
+            catch
+            {
+                //Error message
+                MessageBox.Show("Something went wrong");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void SaveFileButton_Click(object sender, EventArgs e)
+        {
+
+            //Declaring the StreamWriter variable
+            StreamWriter outputFile;
+
+            //Asks the user to save a file
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                //Assigns the file to be saved as the output variable
+                MessageBox.Show("You clicked the Save button.");
+                outputFile = File.CreateText(saveFile.FileName);
+            }
+            else
+            {
+                //Cancel Message
+                MessageBox.Show("you clicked the Cancel button.");
+            }
+        }
+    }
+}
